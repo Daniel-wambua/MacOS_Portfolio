aa59e6a72e99f2590859ea6d23e02f8dda669cbd
Daniel-wambua <wambuahdaniel@gmail.com>
2025-11-25 17:27:45 +0300

    correct some mistakes

--- src/constants/index.js
+++ src/constants/index.js
@@ -278,7 +278,7 @@ const WORK_LOCATION = {
           icon: "/images/safari.png",
           kind: "file",
           fileType: "url",
-          href: "https://youtu.be/fZdTYswuZjU?si=Awjl-pIst9e09_UU",
+          href: "https://nike.com",
           position: "top-10 right-20",
         },
         {
